01446ab9cdb19aeaa8bf2a616b4ba2736a2434f6
xyli <lxy973579512@163.com>
2024-04-25 17:08:48 -0400

    initial commit

--- app/src/main/java/edu/duke/ece651/xl404/javaFx/App.java
+++ app/src/main/java/edu/duke/ece651/xl404/javaFx/App.java
@@ -3,12 +3,31 @@
  */
 package edu.duke.ece651.xl404.javaFx;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+//public class App {
+//  public String getGreeting() {
+//      return "Hello World!";
+//  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+//  public static void main(String[] args) {
+///      System.out.println(new App().getGreeting());
+// }
+//}
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class App extends Application {
+  @Override
+  public void start(Stage stage) {
+    String javaVersion = System.getProperty("java.version");
+    String javafxVersion = System.getProperty("javafx.version");
+    Label l = new Label("Hello, JavaFX " + javafxVersion +
+                        ", running on Java " +
+                        javaVersion + ".");
+    Scene scene = new Scene(new StackPane(l), 640, 480);
+    stage.setScene(scene);
+    stage.show();
+  }
 }
