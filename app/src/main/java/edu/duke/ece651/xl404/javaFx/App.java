98ffd6dba537de3483f18c2fae6d51b0344810b0
xyli <lxy973579512@163.com>
2024-04-26 17:59:44 -0400

    nearly completed

--- app/src/main/java/edu/duke/ece651/xl404/javaFx/App.java
+++ app/src/main/java/edu/duke/ece651/xl404/javaFx/App.java
@@ -3,6 +3,9 @@
  */
 package edu.duke.ece651.xl404.javaFx;
 
+import java.io.IOException;
+import java.net.URL;
+
 //public class App {
 //  public String getGreeting() {
 //      return "Hello World!";
@@ -14,20 +17,19 @@ package edu.duke.ece651.xl404.javaFx;
 //}
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 public class App extends Application {
   @Override
-  public void start(Stage stage) {
-    String javaVersion = System.getProperty("java.version");
-    String javafxVersion = System.getProperty("javafx.version");
-    Label l = new Label("Hello, JavaFX " + javafxVersion +
-                        ", running on Java " +
-                        javaVersion + ".");
-    Scene scene = new Scene(new StackPane(l), 640, 480);
-    stage.setScene(scene);
-    stage.show();
+  public void start(Stage stage) throws IOException{
+   URL xmlResource = getClass().getResource("/ui/calc-monolithic.xml");
+   URL cssResource = getClass().getResource("/ui/calcbuttons.css");
+   GridPane gp = FXMLLoader.load(xmlResource);
+   Scene scene = new Scene(gp, 640, 480);
+   scene.getStylesheets().add(cssResource.toString());
+   stage.setScene(scene);
+   stage.show();
   }
 }
---
+++ app/src/test/java/edu/duke/ece651/xl404/javaFx/controller/NumButtonControllerTest.java
@@ -0,0 +1,49 @@
+package edu.duke.ece651.xl404.javaFx.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.TextInputControlMatchers;
+import org.testfx.util.WaitForAsyncUtils;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+@ExtendWith(ApplicationExtension.class)
+public class NumButtonControllerTest {
+  private TextField testText;
+  private NumButtonController cont;
+  @Start
+  private void start(Stage stage) {
+    testText = new TextField();
+    cont = new NumButtonController();
+    cont.currentNumber = testText;
+  }
+  private void addNums(String ... strs) {
+    Platform.runLater(()->{
+        for (String s: strs){
+          Button b = new Button(s);
+          cont.onNumberButton(new ActionEvent(b,null));
+        }
+      });
+    WaitForAsyncUtils.waitForFxEvents();
+  }
+
+  @Test
+  public void test_onNumberButton_7(FxRobot robot) {
+    addNums("7");
+    FxAssert.verifyThat(testText, TextInputControlMatchers.hasText("7"));
+  }
+  @Test
+  public void test_onNumberButton_pi(FxRobot robot) {
+    addNums("3", ".", "1", "4");
+    FxAssert.verifyThat(testText, TextInputControlMatchers.hasText("3.14"));
+  }
+}
+
